7a0178943d42e1abe939689b3bdb6d6d01fb90ac
BOBINYOURFLOP <82234906+BOBINYOURFLOP@users.noreply.github.com>
2022-11-04 19:08:54 +0000

    Update AddToDatabase.py

--- AddToDatabase.py
+++ AddToDatabase.py
@@ -1,5 +1,18 @@
-import urllib.request, json
-github_link = "https://BOBINYOURFLOP.github.io/Database.json"
-with urllib.request.urlopen(github_link) as url: 
-   json_data = json.loads(1)
-print(json_data)
+from flask import Flask, render_template, request
+
+app = Flask("app")
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+  if request.method == "GET": # if form method is GET
+    text = request.args["text"]
+    return text
+  elif request.method == "POST": # if form method is POST
+    text = request.form["text"]
+    return text
+
+app.run(host="0.0.0.0", port=8080)
