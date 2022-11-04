e9618966e0627838be4c30b805d1ffbf3cdc6a1d
BOBINYOURFLOP <82234906+BOBINYOURFLOP@users.noreply.github.com>
2022-11-04 19:05:35 +0000

    Update AddToDatabase.py

--- AddToDatabase.py
+++ AddToDatabase.py
@@ -1,5 +1,5 @@
 import urllib.request, json
 github_link = "https://BOBINYOURFLOP.github.io/Database.json"
 with urllib.request.urlopen(github_link) as url: 
-   json_data = json.loads(url.read().decode())
+   json_data = json.loads(1)
 print(json_data)
